087d75eaf14f312368f3633edba7abcd42ff857f
RedworkDE <10944644+RedworkDE@users.noreply.github.com>
2023-02-20 11:04:34 +0100

    Initial support for providing generic arguments as comments

--- Disasmo/Utils/SymbolUtils.cs
+++ Disasmo/Utils/SymbolUtils.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Disasmo.Utils;
 
@@ -9,6 +11,7 @@ public static class SymbolUtils
         string target;
         string hostType;
         string methodName;
+        string genericArguments;
 
         if (symbol is IMethodSymbol ms)
         {
@@ -31,12 +34,21 @@ public static class SymbolUtils
                 hostType = symbol.ContainingType.MetadataName();
                 methodName = symbol.Name;
             }
+
+            genericArguments = GenericsForSymbol(symbol);
         }
         else if (symbol is IPropertySymbol prop)
         {
             target = "*" + symbol.ContainingType.MetadataName + ":get_" + symbol.Name + " " + "*" + symbol.ContainingType.MetadataName + ":set_" + symbol.Name;
             hostType = symbol.ContainingType.MetadataName();
             methodName = symbol.Name;
+            genericArguments = (prop.GetMethod, prop.SetMethod) switch
+            {
+                (not null, not null) => GenericsForSymbol(prop.GetMethod, false, false) + GenericsForSymbol(prop.SetMethod),
+                (null, not null) => GenericsForSymbol(prop.GetMethod),
+                (not null, null) => GenericsForSymbol(prop.SetMethod),
+                _ => ""
+            };
         }
         else
         {
@@ -44,8 +56,9 @@ public static class SymbolUtils
             target = symbol.Name + ":*";
             hostType = symbol.MetadataName;
             methodName = "*";
+            genericArguments = GenericsForSymbol(symbol);
         }
-        return new DisasmoSymbolInfo(target, hostType, methodName);
+        return new DisasmoSymbolInfo(target, hostType, methodName, genericArguments);
     }
 
     private static string MetadataName(this INamedTypeSymbol type)
@@ -60,5 +73,59 @@ public static class SymbolUtils
             name = type.ContainingNamespace.ToString() + ".";
         }
         return name + type.MetadataName;
+    }
+
+    private static string GenericsForSymbol(ISymbol symbol, bool includeNested = true, bool includeContaining = true)
+    {
+        const string MARKER = "Disasmo-Generic:";
+
+        if (symbol is null)
+        {
+            return "";
+        }
+
+        string result = "";
+
+        foreach (var syntaxReference in symbol.DeclaringSyntaxReferences)
+        {
+            foreach (var trivia in syntaxReference.GetSyntax().GetLeadingTrivia())
+            {
+                if (!trivia.IsKind(SyntaxKind.SingleLineCommentTrivia) && !trivia.IsKind(SyntaxKind.MultiLineCommentTrivia)) continue;
+
+                var str = trivia.ToFullString().AsSpan();
+                while (!str.IsEmpty)
+                {
+                    var idxStart = str.IndexOf(MARKER.AsSpan(), StringComparison.OrdinalIgnoreCase);
+                    if (idxStart < 0) break;
+                    var len = str.Slice(idxStart + MARKER.Length).IndexOf('\n');
+                    if (len < 0) len = str.Length - idxStart - MARKER.Length;
+                    result = result + SymbolName(symbol) + "=" + str.Slice(idxStart + MARKER.Length, len).Trim().ToString() + ";";
+                    str = str.Slice(idxStart + MARKER.Length + len);
+                }
+            }
+        }
+
+        if (includeContaining)
+        {
+            result += GenericsForSymbol(symbol.ContainingType, false);
+        }
+
+        if (includeNested && symbol is ITypeSymbol typeSymbol)
+        {
+            foreach (var member in typeSymbol.GetMembers())
+            {
+                if (member is IMethodSymbol)
+                {
+                    result += GenericsForSymbol(member, false, false);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static string SymbolName(ISymbol symbol)
+    {
+        return symbol.MetadataName;
     }
 }
